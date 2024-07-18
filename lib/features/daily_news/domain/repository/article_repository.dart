3de08341ba8a1e32081b68938db0d0c93b8534f2
darkrimson <arsanov2003@yandex.ru>
2024-07-18 14:07:34 +0300

    add usecase

--- lib/features/daily_news/domain/repository/article_repository.dart
+++ lib/features/daily_news/domain/repository/article_repository.dart
@@ -1,6 +1,6 @@
 import 'package:news_app/core/resources/data_state.dart';
 import 'package:news_app/features/daily_news/domain/entities/article.dart';
 
-abstract class ArcticleRepository {
+abstract class ArticleRepository {
   Future<DataState<List<ArticleEntity>>> getNewsArticles();
 }
